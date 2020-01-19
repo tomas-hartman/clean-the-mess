585fb7a63e6f0b42f2675d3ecdec743c5de1640e
tomas-hartman <tomas.hartman@hotmail.com>
2020-01-19 21:54:28 +0100

    fix (popup): #23, #14, merge into showScreen

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -87,6 +87,8 @@
         
         tabsOverview.sort((a,b) => b.count-a.count);
 
+        // browser.browserAction.setBadgeText({text: `${tabs.length}`});
+
         return tabsOverview;
     }
     tabsOverview = getOverview(tabs);
@@ -123,7 +125,8 @@
      * Use only after removal!
      * @param {string} data id of selected url from tabsOverview
      */
-    const refreshOverviewData = (data) => {
+    const refreshOverviewData = async (data) => {
+        tabs = await browser.tabs.query({currentWindow: true});
         let currentTabsNum = parseInt(document.querySelector("#open-tabs-count").innerText);
         let tabsNum = tabsOverview[data].ids.length;
         let newTabsNum = currentTabsNum - tabsNum;
@@ -138,14 +141,17 @@
     /**
      * Use only after details to overview transition (pressing back button)
      */
-    const refreshOverviewScreen = async () => {
+    const refreshOverviewScreen = async ({ deletedId } = {}) => {
         document.querySelector("#main-container").classList.add("slide-in-reverse");
         document.querySelector("#details").classList.add("slide-out-reverse");
         
         document.addEventListener("transitionend", () => {
             document.querySelector("#main-container").classList = "";
         }, { once: true });
-        
+
+        if(deletedId) tabs.splice(tabs.findIndex(tab => tab.id === deletedId), 1);
+
+        // tabs.remove() is async
         tabs = await browser.tabs.query({currentWindow: true});
         tabsOverview = getOverview(tabs);
 
@@ -158,21 +164,21 @@
         anchorMain.appendChild(getOverviewList(tabsOverview));
     }
 
-    const removeTabs = (target) => {
+    const removeTabs = async (target) => {
         const data = target.closest("li").dataset.indexNumber;
         const id = tabsOverview[data].ids;
         if(id.length > 10) {
             if(confirm(`Are you sure you want to close ${id.length} tabs?`)){
-                browser.tabs.remove(id);
+                await browser.tabs.remove(id);
                 refreshOverviewData(data);
             } else return;
         } else {
-            browser.tabs.remove(id);
+            await browser.tabs.remove(id);
             refreshOverviewData(data);
         }
     }
 
-    const getDetails = (target) => {
+    const getDetailsArray = (target) => {
         const index = target.closest("li").dataset.indexNumber;
         const ids = tabsOverview[index].ids;
         let array = [];
@@ -197,7 +203,7 @@
         }
     }
 
-    const createSlideScreen = (headerTitle) => {
+    const createSlideScreenBody = (headerTitle) => {
         const headerDivStr = `<div id="header" class="control"><div class="back go-back" title="Back"></div>
                         <div class="header-title">${headerTitle}</div></div>
                         <div class="separator separator-bottom"></div>`;
@@ -217,77 +223,132 @@
         return mainDetailsDiv;
     }
 
-    /**
-     * @todo Merge showLatestDisplayed() with showDetailsScreen()
-     * @param {DOM target} target 
-     */
-    const showLatestDisplayed = (target, numOfLatest = 10) => {
-        const array = getLatestUsed(tabs, numOfLatest);
-        let headerTitle = `${numOfLatest} longest unused tabs`;
+    const bookmarkTab = async (url, title, id) => {
+        const isSupportedProtocol = (url) => {
+            const supportedProtocols = ["https:", "http:", "ftp:", "file:"];
+            const urlObj = new URL(url);
 
-        const screen = createSlideScreen(headerTitle);
-        const ul = screen.querySelector("ul");
-
-        // Fill in with content
-        for (let i = 0; i < array.length; i++) {
-            const text = `
-                <li id="item-${i}" class="detail" data-tab-id="${array[i].id}">
-                    <div class="item-container detail">
-                        <div class="item-text-container">
-                            <div class="title detail" title="${array[i].title}">${array[i].title}</div>
-                            <div class="url detail hidden" title="${array[i].url}">${array[i].url}</div>
-                            <div class="last-displayed detail" title="${array[i].date}">${array[i].date}</div>
-                        </div>
-                        <div class="item-buttons-container">
-                            <div class="bookmark bookmark-close detail" title="Bookmark and close tab"></div>
-                            <div class="remove detail" title="Close tab"></div>
-                        </div>
-                    </div>
-                </li>
-                `;
-            
-        ul.appendChild(document.createRange().createContextualFragment(text));
-        addBookmarkStatus(array[i]);
+            return supportedProtocols.indexOf(urlObj.protocol) != -1; 
         }
 
-        // Set up events
-        screen.onclick = (e) => {
-            if(e.target.classList.contains("back")){
-
-                refreshOverviewScreen();
-
-                document.querySelector("#details").remove();
-                document.querySelector("#main-container").style.left = "0px";
-            }
-            if(e.target.classList.contains("remove")){
-                // remove tab
-                const id = parseInt(e.target.closest("li").dataset.tabId);
-                browser.tabs.remove([id]);
-                e.target.closest("li").remove();
-            }
-            if(e.target.closest("li") && !e.target.classList.contains("remove")){
-                // switchTo tab
-                const id = parseInt(e.target.closest("li").dataset.tabId);
-                browser.tabs.update(id, {active: true});
-            }
+        if(isSupportedProtocol(url)){
+            // check if folder exists and create special folder?
+            await browser.bookmarks.create({title: title, url: url}).catch(err => console.log(err));
+            /**
+             * @todo notification!
+             */
+            console.log("New bookmark created");
         }
-
-        document.querySelector("body").appendChild(screen);
     }
 
-    const showDetailsScreen = async (target) => {
-        let array = getDetails(target);
+    /**
+     * removeTab() use for removing tabs in detailed screens
+     * @param {event} e 
+     * @param {array} array detailed array of tabs 
+     * @param {boolean} autoclose determines if screen closes with last closed item
+     */
+    const removeTab = async (e, array, { autoclose = true } = {}) => {
+        // remove tab
+        const id = parseInt(e.target.closest("li").dataset.tabId);
+                        
+        array.splice(array.findIndex(tab => tab.id === id), 1);
+        await browser.tabs.remove([id]);
+
+        e.target.closest("li").remove();
+
+        // autoclose
+        if(autoclose && array.length === 0){
+            refreshOverviewScreen({ deletedId: id });
+
+            document.querySelector("#details").remove();
+            document.querySelector("#main-container").style.left = "0px";
+        }
+    }
+
+    /**
+     * Closes opened screen
+     */
+    const closeScreen = () => {
+        refreshOverviewScreen();
+
+        document.querySelector("#details").remove();
+        document.querySelector("#main-container").style.left = "0px";
+    }
+
+    /**
+     * Returns headerTitle for secondary screens
+     * @param {string} type normal | latest | ??? 
+     * @param {*} param1 
+     * @returns {string} headerTitle
+     */
+    const getHeaderTitle = (type, {target, count} = {}) => {
         let headerTitle = "";
+        if(type === "normal" && target){
+            try {
+                headerTitle = (new URL(target.querySelector('.url').innerText)).host;
+            } catch (error) {
+                headerTitle = target.querySelector('.url').innerText;
+            }
+        } else if (type === "latest" && count) {
+            headerTitle = `${count} longest unused tabs`;
+        } else {
+            console.log("Error: got wrong params on getHeaderTitle()");
+        }
+
+        return headerTitle;
+    }
+
+    /**
+     * Returns sorted/reduced array for detailed secondary screens
+     * @param {string} type normal | latest | ??? 
+     * @param {*} param1 
+     */
+    const getDetailedArray = (type, {target, count} = {}) => {
+        let array = [];
+        if (type === "normal" && target) {
+            array = getDetailsArray(target);
+        } else if(type === "latest" && count) {
+            array = getLatestUsed(tabs, count);
+        } else {
+            console.log("Error: got wrong params on getDetailedArray()");
+        }
+
+        return array;
+    }
+
+    const setClass = (type, className) => {
+        const dict = {
+            normal: {
+                url: "",
+                lastDisplayed: "hidden"
+            },
+            latest: {
+                url: "hidden",
+                lastDisplayed: ""
+            }
+        }
 
         try {
-            headerTitle = (new URL(target.querySelector('.url').innerText)).host;
-        } catch (error) {
-            headerTitle = target.querySelector('.url').innerText;
+            return dict[type][className];
+        } catch (err){
+            throw err;
         }
-        
-        const screen = createSlideScreen(headerTitle);
-        const ul = screen.querySelector("ul");
+    }
 
+     /**
+      * Main function for generating secondary screens
+      * Replaces showLatestDisplayed() and showDetailedScreen()
+      * @param {DOM target} target 
+      * @param {number} [latestCount = 10] Number of latest shown
+      * @param {string} [type = "normal"] normal | latest | ??? 
+      */
+    const showScreen = (target, {latestCount = 10, type = "normal"} = {}) => {
+        const array = getDetailedArray(type, {count: latestCount, target: target});
+        const headerTitle = getHeaderTitle(type, {count: latestCount, target: target});
+        
+        const screen = createSlideScreenBody(headerTitle);
+        const ul = screen.querySelector("ul");
+    
         // Fill in with content
         for(let i=0; i < array.length; i++){
             const text = `
@@ -295,7 +356,8 @@
                 <div class="item-container detail">
                     <div class="item-text-container">
                         <div class="title detail" title="${array[i].title}">${array[i].title}</div>
-                        <div class="url detail" title="${array[i].url}">${array[i].url}</div>
+                        <div class="url detail ${setClass(type, "url")}" title="${array[i].url}">${array[i].url}</div>
+                        <div class="last-displayed detail ${setClass(type, "lastDisplayed")}" title="${array[i].date}">${array[i].date}</div>
                     </div>
                     <div class="item-buttons-container">
                         <div class="bookmark bookmark-close detail hidden" title="Bookmark and close tab"></div>
@@ -332,28 +394,22 @@
 
         screen.onclick = (e) => {
             if(e.target.classList.contains("back")){
-                refreshOverviewScreen();
+                closeScreen();
+            }
+            if(e.target.classList.contains("bookmark-close")){
+                // bookmark & remove tab
+                const id = parseInt(e.target.closest("li").dataset.tabId);
+                const arrItem = (array.filter(item => item.id === id))[0];
+                const url = arrItem.url;
+                const title = arrItem.title;
 
-                document.querySelector("#details").remove();
-                document.querySelector("#main-container").style.left = "0px";
+                bookmarkTab(url, title, id);
+                removeTab(e, array);
             }
             if(e.target.classList.contains("remove")){
-                // remove tab
-                const id = parseInt(e.target.closest("li").dataset.tabId);
-                
-                array.splice(array.findIndex(tab => tab.id === id), 1);
-                browser.tabs.remove([id]);
-                e.target.closest("li").remove();
-
-                // autoclose
-                if(array.length === 0){
-                    refreshOverviewScreen();
-
-                    document.querySelector("#details").remove();
-                    document.querySelector("#main-container").style.left = "0px";
-                }
+                removeTab(e, array);
             }
-            if(e.target.closest("li") && !e.target.classList.contains("remove")){
+            if(e.target.closest("li") && !e.target.classList.contains("remove") && !e.target.classList.contains("bookmark-close")){
                 // switchTo tab
                 const id = parseInt(e.target.closest("li").dataset.tabId);
                 browser.tabs.update(id, {active: true});
@@ -384,10 +440,9 @@
             console.log("Tabs removed!");
         }
         if( e.target.closest("div.url-container") && !e.target.classList.contains("remove")){
-            // getDetails(e.target);
             const target = e.target.closest("div.url-container");
 
-            showDetailsScreen(target);
+            showScreen(target, {type: "normal"})
             new Promise((resolve, reject) => {
                 setTimeout(() => {
                     resolve();
@@ -400,7 +455,7 @@
             console.log("clicked");
         }
         if(e.target.closest("#ten-unused")){
-            showLatestDisplayed(e.target);
+            showScreen(e.target, {type: "latest"});
 
             new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -412,11 +467,4 @@
             });
         }
     }
-
-    browser.theme.getCurrent().then((a) => {
-        console.log(a);
-    });
-
-    getLatestUsed(tabs);
-
 })();
