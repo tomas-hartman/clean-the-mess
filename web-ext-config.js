6b8a9f06800490eb207565f7c722b03029812013
tomas-hartman <tomas.hartman@hotmail.com>
2020-11-02 23:26:27 +0100

    dev (builder): compatibility build structure

--- web-ext-config.js
+++ web-ext-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	verbose: false,
-	artifactsDir: './build',
-	sourceDir: './src',
+	// artifactsDir: './build',
+	// sourceDir: './src',
 	ignoreFiles: [
 		'./dev',
 		'./icons/unused',
