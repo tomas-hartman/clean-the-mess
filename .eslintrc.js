c7ffe4531c76cadc6f0b6652cb78ea308c036602
tomas-hartman <tomas.hartman@hotmail.com>
2020-11-02 23:24:43 +0100

    feat (project): compatibility extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,15 @@ module.exports = {
 		'webextensions': true,
 		'es2021': true,
 		'browser': true,
-		'node': true
+		'node': true,
+		'jest/globals': true
 	},
 	'extends': 'eslint:recommended',
 	'parserOptions': {
 		'ecmaVersion': 12,
 		'sourceType': 'module'
 	},
+	'plugins': ['jest'],
 	'rules': {
 		'indent': [
 			'error',
