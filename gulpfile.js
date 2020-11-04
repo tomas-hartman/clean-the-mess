5ed6f0eafb439a011f9322253d76ee786ae42419
tomas-hartman <tomas.hartman@hotmail.com>
2020-11-04 01:04:39 +0100

    feat (gulp): scss and autoreload

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,83 @@
 const { watch, src, dest, parallel } = require('gulp');
+const sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 /**
- * Tasks
+ * Build single tasks
  */
-function manifest(browser) {
-	return src('src/*.*').pipe(dest(`dist/${browser}/`));
+function common(browser) {
+	// copy everything except for those with distinct files and dev folders
+	return src(['src/**/*', '!src/icons/**', '!src/styles/**', '!src/dev/**']).pipe(dest(`dist/${browser}/`));
 }
 
-function modules(browser) {
-	return src('src/modules/*').pipe(dest(`dist/${browser}/modules`));
-}
+function styles(browser, _srcPath, _destPath){
 
-function styles(browser) {
-	return src(`src/styles/${browser}/*.*`).pipe(dest(`dist/${browser}/styles/`));
-}
+	console.log(_srcPath);
+	console.log(_destPath);
+	
+	const srcPath = _srcPath || `src/styles/${browser}/**/*.scss`;
+	const destPath = _destPath || `dist/${browser}/styles/`;
+	
+	console.log(srcPath);
+	console.log(destPath);
 
-function background(browser) {
-	return src('src/background/*.*').pipe(dest(`dist/${browser}/background`));
-}
-
-function popup(browser) {
-	return src('src/popup/*.*').pipe(dest(`dist/${browser}/popup`));
+	return src(srcPath)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest(destPath));
 }
 
 function icons(browser) {
 	return src(['src/icons/*.*', '!src/icons/unused/**']).pipe(dest(`dist/${browser}/icons`));
 }
 
+function server() {
+	const express = require('express');
+	const livereload = require('livereload');
+	const connectLivereload = require('connect-livereload');
+
+	const app = express();
+	const port = 3000;
+
+	const liveReloadServer = livereload.createServer();
+	liveReloadServer.watch('src/dev/style-dev');
+
+	app.use(connectLivereload());
+	app.use(express.static('src/dev/style-dev'));
+
+	app.listen(port, () => {
+		console.log(`Started localhost on port ${port}`);
+	});
+}
+
+function devserver() {
+	const styleDevPath = 'src/dev/style-dev';
+	const styleDevSrcPath = 'src/dev/style-dev/**/*.scss';
+
+	styles('', styleDevSrcPath, styleDevPath);
+	// server();
+}
+
 /**
  * Joint function 
  */
-function browser(name){
-	manifest(name);
-	modules(name);
-	styles(name);
-	background(name);
-	popup(name);
-	icons(name);
+function build(browser){
+	styles(browser);
+	icons(browser);
+	common(browser);
 }
 
 /**
  * Environment building
  */
-function firefoxTask(cb) {
-	browser('firefox');
+function buildDevFirefox(cb) {
+	build('firefox');
     
 	cb();
 }
 
-function chromeTask(cb) {
-	browser('chrome');
+function buildDevChrome(cb) {
+	build('chrome');
     
 	cb();
 }
@@ -57,16 +85,30 @@ function chromeTask(cb) {
 /**
  * Exports
  */
-exports.build = parallel(firefoxTask, chromeTask);
+exports.build = parallel(buildDevFirefox, buildDevChrome);
+// exports.devserver = devserver;
+exports.devserver = function(){
+	server();
+	// devserver();
+
+	watch('./src/dev/style-dev', devserver);
+};
 
 exports.firefox = function(){
-	watch('src/', firefoxTask);
+	console.log('Waiting for changes...');
+	// add some cleanup
+	watch(['src/', '!src/dev/'], buildDevFirefox);
 };
 
 exports.chrome = function(){
-	watch('src/', chromeTask);
+	console.log('Waiting for changes...');
+	// add some cleanup
+	watch('src/', buildDevChrome);
 };
 
 exports.default = function(){
-	watch('src/', parallel(firefoxTask, chromeTask));
+	console.log('Waiting for changes...');
+	watch(['src/', '!src/dev/'], parallel(buildDevFirefox, buildDevChrome));
 };
+
+
