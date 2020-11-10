52f6d6ec45b21fd8772265dfd4b94afcffe1d94c
tomas-hartman <tomas.hartman@hotmail.com>
2020-11-11 00:06:44 +0100

    fix (tests): es6 modules and eslint for tests and sample data

--- tests/getDetailsArray.test.js
+++ tests/getDetailsArray.test.js
@@ -1,14 +1,14 @@
-const { getDetailsArray } = require("../src/popup/popup");
+import { getDetailsArray } from '../src/popup/popup';
 
-const { tabs } = require("./__sampleData__/tabs");
-const { tabsOverview } = require("./__sampleData__/tabsOverview");
+import { tabs } from './__sampleData__/tabs';
+import { tabsOverview } from './__sampleData__/tabsOverview';
 
 it('should return array with details only for certain url', () => {
-    const overviewId = 0;
-    const output = getDetailsArray(0, tabsOverview, tabs);
+	const overviewId = 0;
+	const output = getDetailsArray(0, tabsOverview, tabs);
 
-    expect(output).toMatchSnapshot();
-    expect(output.length).toBe(tabsOverview[overviewId].ids.length);
+	expect(output).toMatchSnapshot();
+	expect(output.length).toBe(tabsOverview[overviewId].ids.length);
 });
 
 // vymyslet failcase scénáře
--- tests/getOverview.test.js
+++ tests/getOverview.test.js
@@ -1,143 +1,143 @@
-const { getOverview } = require("../src/popup/popup");
-const { tabs } = require("./__sampleData__/tabs");
+import { getOverview } from '../src/popup/popup';
+import { tabs } from './__sampleData__/tabs';
 
 it('should return correct overview data', () => {
-    const output = getOverview(tabs);
+	const output = getOverview(tabs);
 
-    expect(output).toMatchSnapshot();
+	expect(output).toMatchSnapshot();
 });
 
 it('overview should have valid properties', () => {
-    const sampleMinifiedTabs = [
-        {
-            "id": 5,
-            "index": 0,
-            "windowId": 1,
-            "pinned": false,
-            "status": "complete",
-            "lastAccessed": 1589322068715,
-            "audible": false,
-            "mutedInfo": {
-              "muted": false
-            },
-            "sharingState": {
-              "camera": false,
-              "microphone": false
-            },
-            "successorTabId": -1,
-            "url": "https://www.google.com/?q=1",
-            "title": "Google jedna",
-          },
-        {
-            "id": 7,
-            "index": 1,
-            "windowId": 1,
-            "pinned": false,
-            "status": "complete",
-            "lastAccessed": 1589831142586,
-            "audible": false,
-            "mutedInfo": {
-              "muted": false
-            },
-            "sharingState": {
-              "camera": false,
-              "microphone": false
-            },
-            "successorTabId": -1,
-            "url": "https://www.google.com/?q=2",
-            "title": "Google 2",
-          },
-        {
-            "id": 15,
-            "index": 2,
-            "windowId": 1,
-            "pinned": false,
-            "status": "complete",
-            "lastAccessed": 1589831456789,
-            "audible": false,
-            "mutedInfo": {
-              "muted": false
-            },
-            "sharingState": {
-              "camera": false,
-              "microphone": false
-            },
-            "successorTabId": -1,
-            "url": "https://www.example.com/?q=1",
-            "title": "Example 1",
-          },
-    ]
-    const output = getOverview(sampleMinifiedTabs);
-    const item = output[0];
+	const sampleMinifiedTabs = [
+		{
+			'id': 5,
+			'index': 0,
+			'windowId': 1,
+			'pinned': false,
+			'status': 'complete',
+			'lastAccessed': 1589322068715,
+			'audible': false,
+			'mutedInfo': {
+				'muted': false
+			},
+			'sharingState': {
+				'camera': false,
+				'microphone': false
+			},
+			'successorTabId': -1,
+			'url': 'https://www.google.com/?q=1',
+			'title': 'Google jedna',
+		},
+		{
+			'id': 7,
+			'index': 1,
+			'windowId': 1,
+			'pinned': false,
+			'status': 'complete',
+			'lastAccessed': 1589831142586,
+			'audible': false,
+			'mutedInfo': {
+				'muted': false
+			},
+			'sharingState': {
+				'camera': false,
+				'microphone': false
+			},
+			'successorTabId': -1,
+			'url': 'https://www.google.com/?q=2',
+			'title': 'Google 2',
+		},
+		{
+			'id': 15,
+			'index': 2,
+			'windowId': 1,
+			'pinned': false,
+			'status': 'complete',
+			'lastAccessed': 1589831456789,
+			'audible': false,
+			'mutedInfo': {
+				'muted': false
+			},
+			'sharingState': {
+				'camera': false,
+				'microphone': false
+			},
+			'successorTabId': -1,
+			'url': 'https://www.example.com/?q=1',
+			'title': 'Example 1',
+		},
+	];
+	const output = getOverview(sampleMinifiedTabs);
+	const item = output[0];
 
-    expect(output).toBeInstanceOf(Array);
-    expect(output.length).toBe(2); // because 2 x google and 1 x example
+	expect(output).toBeInstanceOf(Array);
+	expect(output.length).toBe(2); // because 2 x google and 1 x example
 
-    expect(item).toBeInstanceOf(Object);
-    expect(item).toHaveProperty("url");
-    expect(item).toHaveProperty("count");
-    expect(item).toHaveProperty("ids", );
+	expect(item).toBeInstanceOf(Object);
+	expect(item).toHaveProperty('url');
+	expect(item).toHaveProperty('count');
+	expect(item).toHaveProperty('ids', );
 
-    expect(item.ids).toBeInstanceOf(Array);
-    expect(item.ids.length).toBe(item.count);
+	expect(item.ids).toBeInstanceOf(Array);
+	expect(item.ids.length).toBe(item.count);
 
-    expect(item.url).toBe("https://www.google.com");
+	expect(item.url).toBe('https://www.google.com');
 });
 
 describe('Output of url/title', () => {
-    it.each([
-        ["about:", {url: "about:debugging", title: "about:debugging"}, "Browser tabs"],
-        ["moz-extension:", {url: "moz-extension://58ee5ecd-6e4e-4ff4-8d70-56bca7d26444", title: "manifest.json"}, "Browser tabs"],
-        ["chrome:", {url: "chrome://extensions/", title: "chrome://extensions/"}, "Browser tabs"],
-        ["file:", {url: "file:///C:/clean-the-mess/index.html", title: "Test report"}, "Opened files"],
-        ["ftp:", {url: "ftp://www.example.com/index.html", title: "Test report"}, "ftp://www.example.com"],
-        ["localhost", {url: "localhost", title: "localhost"}, "Localhost"],
-        ["localhost:3000", {url: "localhost:3000", title: "localhost:3000"}, "Localhost"],
-        ["IP", {url: "127.0.0.1", title: "IP address"}, "127.0.0.1"],
-        ["IP:port", {url: "127.0.0.1:3000", title: "IP address with port"}, "127.0.0.1"],
-        // Failcases:
-        ["random string", {url: "random string", title: "random string"}, "Other tabs"],
-        ["empty string", {url: "", title: ""}, "Other tabs"],
-    ])(
-        'output should work fine with non-urls - %s',
-        (_tcTitle, testData, expected) => {
-            let { url, title } = testData;
-            const sampleMinifiedTabs = [
-                {
-                    "id": 5,
-                    "index": 0,
-                    "windowId": 1,
-                    "pinned": false,
-                    "status": "complete",
-                    "lastAccessed": 1589322068712,
-                    "url": url,
-                    "title": title,
-                }
-            ];
+	it.each([
+		['about:', {url: 'about:debugging', title: 'about:debugging'}, 'Browser tabs'],
+		['moz-extension:', {url: 'moz-extension://58ee5ecd-6e4e-4ff4-8d70-56bca7d26444', title: 'manifest.json'}, 'Browser tabs'],
+		['chrome:', {url: 'chrome://extensions/', title: 'chrome://extensions/'}, 'Browser tabs'],
+		['file:', {url: 'file:///C:/clean-the-mess/index.html', title: 'Test report'}, 'Opened files'],
+		['ftp:', {url: 'ftp://www.example.com/index.html', title: 'Test report'}, 'ftp://www.example.com'],
+		['localhost', {url: 'localhost', title: 'localhost'}, 'Localhost'],
+		['localhost:3000', {url: 'localhost:3000', title: 'localhost:3000'}, 'Localhost'],
+		['IP', {url: '127.0.0.1', title: 'IP address'}, '127.0.0.1'],
+		['IP:port', {url: '127.0.0.1:3000', title: 'IP address with port'}, '127.0.0.1'],
+		// Failcases:
+		['random string', {url: 'random string', title: 'random string'}, 'Other tabs'],
+		['empty string', {url: '', title: ''}, 'Other tabs'],
+	])(
+		'output should work fine with non-urls - %s',
+		(_tcTitle, testData, expected) => {
+			let { url, title } = testData;
+			const sampleMinifiedTabs = [
+				{
+					'id': 5,
+					'index': 0,
+					'windowId': 1,
+					'pinned': false,
+					'status': 'complete',
+					'lastAccessed': 1589322068712,
+					'url': url,
+					'title': title,
+				}
+			];
 
-            const output = getOverview(sampleMinifiedTabs);
-            const item = output[0];
+			const output = getOverview(sampleMinifiedTabs);
+			const item = output[0];
 
-            expect(item.url).toBe(expected);
-        }
-    );
+			expect(item.url).toBe(expected);
+		}
+	);
 });
 
 it('should skip pinned items', () => {
-    const sampleMinifiedTabs = [
-        {
-            "id": 6,
-            "index": 0,
-            "windowId": 1,
-            "pinned": true,
-            "status": "complete",
-            "lastAccessed": 1589322068733,
-            "url": "https://www.google.com",
-            "title": "Google.com but pinned",
-        }
-    ];
+	const sampleMinifiedTabs = [
+		{
+			'id': 6,
+			'index': 0,
+			'windowId': 1,
+			'pinned': true,
+			'status': 'complete',
+			'lastAccessed': 1589322068733,
+			'url': 'https://www.google.com',
+			'title': 'Google.com but pinned',
+		}
+	];
 
-    const output = getOverview(sampleMinifiedTabs);
+	const output = getOverview(sampleMinifiedTabs);
 
-    expect(output.length).toBe(0);
+	expect(output.length).toBe(0);
 });
