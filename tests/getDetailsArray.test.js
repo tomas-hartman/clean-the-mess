2f7661c54d550753dd94f97f40e7ca0ddac2a2c1
Tomas Hartman <tomash@xcroco.com>
2021-05-08 13:08:09 +0200

    refactor (tests): eslint

--- tests/getDetailsArray.test.js
+++ tests/getDetailsArray.test.js
@@ -4,11 +4,11 @@ import { tabs } from './__sampleData__/tabs';
 import { tabsOverview } from './__sampleData__/tabsOverview';
 
 it('should return array with details only for certain url', () => {
-	const overviewId = 0;
-	const output = getDetailsArray(0, tabsOverview, tabs);
+  const overviewId = 0;
+  const output = getDetailsArray(0, tabsOverview, tabs);
 
-	expect(output).toMatchSnapshot();
-	expect(output.length).toBe(tabsOverview[overviewId].ids.length);
+  expect(output).toMatchSnapshot();
+  expect(output.length).toBe(tabsOverview[overviewId].ids.length);
 });
 
 // vymyslet failcase scénáře
--- tests/getLatestUsed.test.js
+++ tests/getLatestUsed.test.js
@@ -2,53 +2,53 @@ import { getLatestUsed } from '../src/modules/details';
 import { tabs } from './__sampleData__/tabs';
 
 it('should return formated array', () => {
-	const output = getLatestUsed(tabs, 5);
+  const output = getLatestUsed(tabs, 5);
 
-	expect(output).toMatchSnapshot();
-	expect(output.length).toBe(5);
+  expect(output).toMatchSnapshot();
+  expect(output.length).toBe(5);
 });
 
 it('should return formated array of 10', () => {
-	const output = getLatestUsed(tabs);
+  const output = getLatestUsed(tabs);
 
-	expect(output.length).toBe(10);
+  expect(output.length).toBe(10);
 });
 
 it('should not fail if getLatestCount is bigger than tabs.length', () => {
-	const getLatestCount = tabs.length + 10;
-	const output = getLatestUsed(tabs, getLatestCount);
+  const getLatestCount = tabs.length + 10;
+  const output = getLatestUsed(tabs, getLatestCount);
 
-	expect(output).toMatchSnapshot();
-	expect(output.length).toBe(tabs.length);
+  expect(output).toMatchSnapshot();
+  expect(output.length).toBe(tabs.length);
 });
 
 it('should skip pinned items', () => {
-	const sampleMinifiedTabs = [
-		{
-			'id': 6,
-			'index': 0,
-			'windowId': 1,
-			'pinned': true,
-			'status': 'complete',
-			'lastAccessed': 1589322068733,
-			'url': 'https://www.google.com',
-			'title': 'Google.com but pinned',
-		},
-		{
-			'id': 7,
-			'index': 1,
-			'windowId': 1,
-			'pinned': false,
-			'status': 'complete',
-			'lastAccessed': 1589322068733,
-			'url': 'https://www.google.com',
-			'title': 'Google.com',
-		},
+  const sampleMinifiedTabs = [
+    {
+      id: 6,
+      index: 0,
+      windowId: 1,
+      pinned: true,
+      status: 'complete',
+      lastAccessed: 1589322068733,
+      url: 'https://www.google.com',
+      title: 'Google.com but pinned',
+    },
+    {
+      id: 7,
+      index: 1,
+      windowId: 1,
+      pinned: false,
+      status: 'complete',
+      lastAccessed: 1589322068733,
+      url: 'https://www.google.com',
+      title: 'Google.com',
+    },
 
-	];
+  ];
 
-	const output = getLatestUsed(sampleMinifiedTabs, 10);
+  const output = getLatestUsed(sampleMinifiedTabs, 10);
 
-	expect(output).toMatchSnapshot();
-	expect(output.length).toBe(1);
+  expect(output).toMatchSnapshot();
+  expect(output.length).toBe(1);
 });
