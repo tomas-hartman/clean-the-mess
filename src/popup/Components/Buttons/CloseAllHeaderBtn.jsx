a0ea9f77b81d21c08da23be4c9ebf4813146c7e9
Tomas Hartman <tomash@xcroco.com>
2021-05-28 00:40:18 +0200

    refactor (Confirm): fix callWithConfirm and build

--- src/popup/Components/Buttons/CloseAllHeaderBtn.jsx
+++ src/popup/Components/Buttons/CloseAllHeaderBtn.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { callWithConfirm } from '../../../modules/helpers.refactor';
 
-export default function CloseAllHeaderBtn({ onClick }) {
+export default function CloseAllHeaderBtn({ onClick, itemCount }) {
+  const handleClick = () => {
+    if (itemCount > 15) {
+      callWithConfirm('closeTabs', onClick, () => true, itemCount);
+      return;
+    }
+
+    onClick();
+  };
+
   return (
-    <button type="button" className="close-all" title="Close all listed tabs" onClick={onClick}>
+    <button type="button" className="close-all" title="Close all listed tabs" onClick={handleClick}>
       <span className="hidden">Close all listed tabs</span>
     </button>
   );
--- src/popup/Components/Details/DetailsHeader.jsx
+++ src/popup/Components/Details/DetailsHeader.jsx
@@ -12,7 +12,10 @@ export default function DetailsHeader(props) {
       <div id="header" className="control">
         <GoBackBtn handleClick={() => switchToScreen('overview')} />
         <div className="header-title">{title}</div>
-        <CloseAllHeaderBtn onClick={() => closeTabs(overviewData.ids)} />
+        <CloseAllHeaderBtn
+          onClick={() => closeTabs(overviewData?.ids)}
+          itemCount={overviewData?.ids.length}
+        />
       </div>
       <Separator />
     </div>
