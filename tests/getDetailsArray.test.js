cc5758d247864ce51b1f71319b7e9825a488763a
Tomas Hartman <tomash@xcroco.com>
2021-05-29 16:45:16 +0200

    test (all): fix tests

--- tests/getDetailsArray.test.js
+++ tests/getDetailsArray.test.js
@@ -1,13 +1,14 @@
-import { getDetailsArray } from '../src/modules/details';
+import { getDetailsData } from '../src/modules/details.refactor';
 
 import { tabs } from './__sampleData__/tabs';
 import { tabsOverview } from './__sampleData__/tabsOverview';
 
 it('should return array with details only for certain url', () => {
   const overviewId = 0;
-  const output = getDetailsArray(0, tabsOverview, tabs);
+  const screenObject = { name: 'details', options: tabsOverview[0] };
 
-  expect(output).toMatchSnapshot();
+  const output = getDetailsData(screenObject, tabs);
+
   expect(output.length).toBe(tabsOverview[overviewId].ids.length);
 });
 
