275d6de65f78b475fcbeda4b1591252442f9c149
Tomas Hartman <tomas.hartman@hotmail.com>
2023-03-18 01:24:26 +0100

    fix (types): add typing for svg

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,27 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: (webpackConfig) => {
+    if(webpackConfig?.module?.rules) {
+      // This modifies the existing image rule to exclude `.svg` files
+      // since we handle those with `@svgr/webpack`.
+      const imageRule = webpackConfig.module.rules.find((rule) => {
+        if (typeof rule !== 'string' && rule.test instanceof RegExp) {
+          return rule.test.test('.svg')
+        }
+      })
+      
+      if (imageRule && typeof imageRule !== 'string') {
+        imageRule.exclude = /\.svg$/
+      }
+
+      webpackConfig.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+    }
+
+    return webpackConfig;
+  }
 };
 export default config;
