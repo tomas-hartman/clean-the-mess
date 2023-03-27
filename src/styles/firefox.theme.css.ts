2775a2a396d915c0dade33a07dc696b4ae9a93f8
Tomas Hartman <tomas.hartman@hotmail.com>
2023-03-27 23:27:17 +0200

    feat (theme): add theme for color schemes in firefox

--- src/styles/firefox.theme.css.ts
+++ src/styles/firefox.theme.css.ts
@@ -1,37 +1,73 @@
-import { createTheme } from "@vanilla-extract/css";
-import { themeContract } from "./themes.css";
+import { createGlobalTheme } from "@vanilla-extract/css";
+import { themeColorContract, themeUtilsContract } from "./themes.css";
 
-// TODO: ADD DARK THEME
-export const themeFirefox = createTheme(themeContract, {
+export const themeFirefoxLightScheme = createGlobalTheme(":root", themeColorContract, {
   color: {
-    font: "#0c0c0d",
-    fontSecondary: "#737373",
-    fontAlternative: "#0c0c0d",
+    colorFont: "#0c0c0d",
+    colorFontSecondary: "#737373",
+    colorFontAlternative: "#0c0c0d",
 
-    background: "#ffffff",
-    backgroundSecondary: "#ffffff",
-    backgroundAlternative: "#ffffff",
+    colorBackground: "#ffffff",
+    colorBackgroundSecondary: "#ffffff",
+    colorBackgroundAlternative: "#ffffff",
 
-    separator: "#d7d7db",
+    colorSeparator: "#d7d7db",
 
-    emphasisDark: "#d7d7dbdc",
-    emphasisLight: "#ededf0d0",
+    colorEmphasisDark: "#d7d7dbdc",
+    colorEmphasisLight: "#ededf0d0",
 
-    linkHover: "#d7d7dbdc",
-    linkHoverPurple: "#c169ffb9",
+    colorLinkHover: "#d7d7dbdc",
+    colorLinkHoverPurple: "#c169ffb9",
 
-    backButton: "#ffffff",
+    colorBackButton: "#ffffff",
 
-    buttonFont: "#0c0c0dcc",
-    buttonHover: "#ededf0d0", // emphasis light
-    buttonActive: "#d7d7dbdc", // emphasis dark
+    colorButtonFont: "#0c0c0dcc",
+    colorButtonHover: "#ededf0d0", // emphasis light
+    colorButtonActive: "#d7d7dbdc", // emphasis dark
 
-    backgroundHeaderContainer: "inherit",
-    searchContainer: "#0c0c0d", // var(--color-alternative-font)
+    colorBackgroundHeaderContainer: "inherit",
+    colorSearchContainer: "#0c0c0d", // var(--color-alternative-font)
+    colorSearchInputColor: "#0c0c0d",
 
-    liCount: "inherit",
-    liHover: "inherit",
+    colorLiCount: "inherit",
+    colorLiHover: "inherit",
   },
+})
+
+export const themeFirefoxDarkScheme = createGlobalTheme(":root", themeColorContract, {
+  color: {
+    "colorFont": "#ffffff",
+    "colorFontSecondary": "#f9f9fa",
+    "colorFontAlternative": "#0c0c0d",
+
+    "colorBackground": "#4a4a4f",
+    "colorBackgroundSecondary": "var(--color-background)",
+    "colorBackgroundAlternative": "rgb(255, 255, 255)",
+
+    "colorSeparator": "#7373738a",
+
+    "colorEmphasisDark": "#737373c1",
+    "colorEmphasisLight": "#7373738a",
+
+    "colorLinkHover": "#663399",
+    colorLinkHoverPurple: "#c169ffb9",
+
+    "colorBackButton": "darkorchid",
+
+    "colorButtonFont": "#f9f9facc",
+    colorButtonHover: "var(--color-emphasis-light)",
+    colorButtonActive: "var(--color-emphasis-dark)",
+
+    colorBackgroundHeaderContainer: "inherit",
+    colorSearchContainer: "var(--color-alternative-font)",
+    colorSearchInputColor: "var(--color-alternative-font)",
+
+    colorLiCount: "inherit",
+    colorLiHover: "inherit",
+  }
+})
+
+export const themeFirefoxUtils = createGlobalTheme(":root", themeUtilsContract, {
   font: {
     fontSize: "inherit",
     fontFamily: "inherit",
@@ -43,7 +79,6 @@ export const themeFirefox = createTheme(themeContract, {
     searchBorderRadius: "1px",
     searchBorder: "none",
     searchPaddingLeft: "8px",
-    searchInputColor: "#0c0c0d",
 
     largeButtonSize: "32px",
     smallButtonSize: "20px",
--- src/styles/themes.css.ts
+++ src/styles/themes.css.ts
@@ -1,53 +1,108 @@
-import { createThemeContract } from "@vanilla-extract/css";
+import { createGlobalThemeContract } from "@vanilla-extract/css";
 
-export const themeContract = createThemeContract({
+// Changes on light-dark scheme axis
+export const themeColorContract = createGlobalThemeContract({
   color: {
-    font: null,
-    fontSecondary: null,
-    fontAlternative: null,
+    colorFont: 'color-font',
+    colorFontSecondary: 'color-font-secondary',
+    colorFontAlternative: 'color-font-alternative',
+    colorBackground: 'color-background',
+    colorBackgroundSecondary: 'color-background-secondary',
+    colorBackgroundAlternative: 'color-background-alternative',
 
-    background: null,
-    backgroundSecondary: null,
-    backgroundAlternative: null,
+    colorSeparator: 'color-separator',
 
-    separator: null,
+    colorEmphasisDark: 'color-emphasis-dark',
+    colorEmphasisLight: 'color-emphasis-light',
 
-    emphasisDark: null,
-    emphasisLight: null,
+    colorLinkHover: 'color-link-hover',
+    colorLinkHoverPurple: 'color-link-hover-purple',
 
-    linkHover: null,
-    linkHoverPurple: null,
+    colorBackButton: 'color-back-button',
 
-    backButton: null,
+    colorButtonFont: 'color-button-font',
+    colorButtonHover: 'color-button-hover',
+    colorButtonActive: 'color-button-active',
 
-    buttonFont: null,
-    buttonHover: null,
-    buttonActive: null,
+    colorBackgroundHeaderContainer: 'color-background-header-container',
+    colorSearchContainer: 'color-search-container',
+    colorSearchInputColor: 'color-search-input-color',
 
-    backgroundHeaderContainer: null,
-    searchContainer: null,
-
-    liCount: null,
-    liHover: null,
-  },
-  font: {
-    fontSize: null,
-    fontFamily: null,
-  },
-  radii: {
-    borderRadius: null,
-  },
-  components: {
-    searchBorderRadius: null,
-    searchBorder: null,
-    searchPaddingLeft: null,
-    searchInputColor: null,
-
-    largeButtonSize: null,
-    smallButtonSize: null,
-  },
-  misc: {
-    browser: null,
+    colorLiCount: 'color-li-count',
+    colorLiHover: 'color-li-hover',
   }
 });
+
+// Changes only on browser axis
+export const themeUtilsContract = createGlobalThemeContract({
+  font: {
+    fontSize: 'font-size',
+    fontFamily: 'font-family',
+  },
+  radii: {
+    borderRadius: 'border-radius',
+  },
+  components: {
+    searchBorderRadius: 'search-border-radius',
+    searchBorder: 'search-border',
+    searchPaddingLeft: 'search-padding-left',
+
+    largeButtonSize: 'large-button-size',
+    smallButtonSize: 'small-button-size',
+  },
+  misc: {
+    browser: 'browser',
+  }
+})
+
+// export const themeContract = createGlobalThemeContract({
+//   color: {
+//     font: null,
+//     fontSecondary: null,
+//     fontAlternative: null,
+
+//     background: null,
+//     backgroundSecondary: null,
+//     backgroundAlternative: null,
+
+//     separator: null,
+
+//     emphasisDark: null,
+//     emphasisLight: null,
+
+//     linkHover: null,
+//     linkHoverPurple: null,
+
+//     backButton: null,
+
+//     buttonFont: null,
+//     buttonHover: null,
+//     buttonActive: null,
+
+//     backgroundHeaderContainer: null,
+//     searchContainer: null,
+
+//     liCount: null,
+//     liHover: null,
+//   },
+//   font: {
+//     fontSize: null,
+//     fontFamily: null,
+//   },
+//   radii: {
+//     borderRadius: null,
+//   },
+//   components: {
+//     searchBorderRadius: null,
+//     searchBorder: null,
+//     searchPaddingLeft: null,
+//     searchInputColor: null,
+
+//     largeButtonSize: null,
+//     smallButtonSize: null,
+//   },
+//   misc: {
+//     browser: null,
+//   }
+// });
 
