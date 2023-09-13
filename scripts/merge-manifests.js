8ec48c27de44693017c5f3069a4dea0908c26d9d
Tomas Hartman <tomas.hartman@hotmail.com>
2023-09-14 00:48:10 +0200

    refactor (packages): improve scripts

--- scripts/merge-manifests.js
+++ scripts/merge-manifests.js
@@ -1,6 +1,10 @@
 const jq = require('node-jq');
 const path = require('path');
-const argv = require('minimist')(process.argv.slice(2));
+const { argv } = require('yargs');
+
+if (!argv.f) {
+  throw new Error('Input manifest must be specified with -f flag');
+}
 
 const sourceManifest = path.resolve(__dirname, '../packages/common/src/manifest.common.json');
 const browserManifest = path.resolve(process.cwd(), argv.f);
