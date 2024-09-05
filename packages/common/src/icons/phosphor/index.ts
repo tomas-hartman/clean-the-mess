959f7bf973cf36203860ecbb85df3150481afdfa
Tomas Hartman <tomas.hartman@hotmail.com>
2024-09-05 22:59:41 +0200

    feat (icon): add close all tabs icon

--- packages/common/src/icons/phosphor/index.ts
+++ packages/common/src/icons/phosphor/index.ts
@@ -4,9 +4,13 @@ import BoldPushPin from './bold/push-pin.svg';
 import BoldLockOpen from './bold/lock-open.svg';
 import BoldPushPinSlash from './bold/push-pin-slash.svg';
 import BoldFolders from './bold/folders.svg';
+import CloseAllTabs from './custom-close-all-tabs.svg';
+import CloseSingleTab from './custom-close-single-tab.svg';
 
 export const phosphor = {
   ClockCounterClockwise,
+  CloseAllTabs,
+  CloseSingleTab,
   BoldClockCounterClockwise,
   BoldPushPin,
   BoldLockOpen,
